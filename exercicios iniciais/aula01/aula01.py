a5cdd74504d44db6818b7dc1c90302cf7d95c0ec
Adriana Viaboni <adrianaviabl@yahoo.com.br>
2020-09-29 22:00:29 -0300

    aula 10
    até a aula 10 - variaveis

---
+++ exercicios iniciais/aula01/aula01.py
@@ -0,0 +1,6 @@
+print('ola mundo')  # comentário linear
+print(123456)
+print('python é legal')
+"""
+comentário em bloco
+"""
---
+++ exercicios iniciais/aula02/aula02.py
@@ -0,0 +1,3 @@
+print(123456)
+print('Adriana', 'aprendendo', 'python', end=' ')
+print('trocador de espaços', '\o/', sep='-')  # substituindo o espaço entre as palavras por traço
