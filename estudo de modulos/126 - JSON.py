c4095a5f004f3f01215792239bce1a9a2da7d414
Adriana Viaboni <adrianaviabl@yahoo.com.br>
2020-10-29 10:34:45 -0300

    ate a aula 130 - string
    ate a aula 130 utilização de string em template

--- estudo de modulos/126 - JSON.py
+++ estudo de modulos/126 - JSON.py
@@ -27,3 +27,26 @@ true	        True
 false	        False
 null	        None
 """
+from JSON_dados import *
+import json
+
+print(clientes_dicionario)
+print('convertendo um dicionario em um objeto JSON')
+convertido_json = json.dumps(clientes_dicionario, indent=4)
+print(convertido_json)
+print(type(convertido_json))
+print(10 * "====")
+
+dicionario = json.loads(clientes_json)
+for chave, valor in dicionario.items():
+    print(chave)
+    print(valor)
+
+print('trabalhando com arquivo')
+with open('clietes.json', 'w') as arquivo:
+    json.dump(clientes_dicionario, arquivo, indent=4)
+
+with open('clietes.json', 'r') as arquivo:
+    dados = json.load(arquivo)
+print('arquivo convertido de volta em dicionario')
+print(dados)
